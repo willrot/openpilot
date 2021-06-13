e6b2c3c7c315d58470abce8475021dd918d0fd8f
vanillagorillaa <ntmccoy@yahoo.com>
2021-06-13 19:37:33 +0000

    0.8.5 fixes

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -262,9 +262,9 @@ class CarState(CarStateBase):
       self.brake_error = 0
     else:
       if self.CP.carFingerprint == CAR.CLARITY:
-          self.brake_error = cp.vl["BRAKE_ERROR"]['BRAKE_ERROR_1'] or cp.vl["BRAKE_ERROR"]['BRAKE_ERROR_2']
+          self.brake_error = cp.vl["BRAKE_ERROR"]["BRAKE_ERROR_1"] or cp.vl["BRAKE_ERROR"]["BRAKE_ERROR_2"]
       else:
-          self.brake_error = cp.vl["STANDSTILL"]['BRAKE_ERROR_1'] or cp.vl["STANDSTILL"]['BRAKE_ERROR_2']
+          self.brake_error = cp.vl["STANDSTILL"]["BRAKE_ERROR_1"] or cp.vl["STANDSTILL"]["BRAKE_ERROR_2"]
     ret.espDisabled = cp.vl["VSA_STATUS"]["ESP_DISABLED"] != 0
 
     speed_factor = SPEED_FACTOR[self.CP.carFingerprint]
